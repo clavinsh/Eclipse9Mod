57ec01c27234f0973dd2bdc366f51b3efdecd3cf
clavinsh <klavins.ar@gmail.com>
2024-04-14 13:45:18 +0300

    updated methods for buff index retrieval

--- ExamplePlugin/ExamplePlugin.cs
+++ ExamplePlugin/ExamplePlugin.cs
@@ -5,6 +5,7 @@ using RoR2;
 using System;
 using System.Linq;
 using System.Security.Permissions;
+using UnityEngine;
 
 
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
@@ -25,57 +26,36 @@ namespace ExamplePlugin
         public const string PluginVersion = "1.0.0";
 
 
-        private const string PermanentCurseDebuffInternalName = "PermanentCurse";
+        private const string PermanentCurseDeBuffInternalName = "PermanentCurse";
 
         public static DifficultyIndex myIndex;
+
+
+        private int curseStacks = 0;
 
 
         public void Awake()
         {
             Logger.LogMessage("TESTING 123 my mod supposedly loaded");
-
-            DifficultyDef myDef = new(
-            10f, //This is the scaling factor, and decides how quickly the difficulty ramps up. drizzle is 1, rainstorm=2, monsoon=3.
-            "Lobotomy",//The name token. consider using AssetPlus.Language to add your tokens.
-            "", //The iconPath, You can use a vanilla icon, or with use of AssetAPI/ResourceAPI use your own custom one.
-            "Lobotomy difficulty, totally unfair scaling, 10x monsoon",//The description token. consider using AssetPlus.Language to add your tokens.
-            new UnityEngine.Color(0.5f, 0.1f, 0.2f), //The color that appears when hovering over this in the rulebook.
-            "serverTag??",
-            true // if beaten, unlocks Survivor's Mastery skin
-
-            );
-
-            //sets the diff index to positive, will position this custom diff after eclipse diffs, applies the eclipse modifiers to it
-
-            bool preferPositive = true;
-
-            myIndex = R2API.DifficultyAPI.AddDifficulty(myDef, preferPositive);
-
-            RoR2.Run.onRunStartGlobal += (RoR2.Run run) =>
-            {
-                if (run.selectedDifficulty == myIndex)
-                {
-                    ChatMessage.Send("Lobotomy difficulty selected"!);
-                    //Ideally you'd hook your methods here, so you don't need to check in your changes if the current run is selected. 
-                }
-            };
-            RoR2.Run.onRunDestroyGlobal += (RoR2.Run run) =>
-            {
-                if (run.selectedDifficulty == myIndex)
-                {
-                    //If you made changes to the game, specifically for your own difficulty, this would be the ideal moment to undo them and return the game to a vanilla state.
-                }
-            };
-
-
-
         }
 
-        private void Stage_Start(On.RoR2.Stage.orig_Start orig, Stage self)
+        public void Update()
         {
+            // save permanent curse stacks
+            if(Input.GetKeyDown(KeyCode.F2))
+            {
+                curseStacks = PlayerCharacterMasterController.instances[0].body.GetBuffCount(RoR2Content.Buffs.PermanentCurse.buffIndex);
 
+                Logger.LogMessage($"{curseStacks} Permanent Curse stacks saved");
+            }
 
-            orig(self);
+            // load saved permanent curse stacks
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                PlayerCharacterMasterController.instances[0].body.SetBuffCount(RoR2Content.Buffs.PermanentCurse.buffIndex, curseStacks);
+
+                Logger.LogMessage($"{curseStacks} Permanent Curse applied to player");
+            }
         }
     }
 }
